39ab6b488aed308dd2da433f4d7c819929bb52ce
mikequentel <mikequentel@yahoo.com>
2026-03-25 22:42:55 -0400

    fixup! add tarball_images.sh

--- tarball_images.sh
+++ tarball_images.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
-touch mender_images.tar
-for i in $(docker images --format "{{.Repository}}:{{.Tag}}" | grep -i mender); do docker image save ${i} >> mender_images.tar; done
+mapfile -t images < <(
+  docker images --format '{{.Repository}}:{{.Tag}}' | grep -i mender
+)
 
-docker image save mongo:7.0 >> mender_images.tar
-docker image save chrislusf/seaweedfs:latest >> mender_images.tar
-docker image save traefik:v3.1 >> mender_images.tar
-docker image save nats:2.10 >> mender_images.tar
+images+=(
+  mongo:7.0
+  chrislusf/seaweedfs:latest
+  traefik:v3.1
+  nats:2.10
+)
+
+docker image save -o mender_images.tar "${images[@]}"
 
